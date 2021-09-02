1c78a3828a445457dfc72a2e0270d2bc90a1395b
RafidMuhymin <rafidmuhymin@gmail.com>
2021-09-02 13:28:15 -0400

    removed the modules

--- navigate.js
+++
@@ -1,8 +0,0 @@
-export default function () {
-  return `w.navigate = (href) => {
-    if (l.href !== href && l.pathname !== href) {
-      history.pushState({}, d.title, href);
-      constructPage();
-    }
-  };`;
-}
