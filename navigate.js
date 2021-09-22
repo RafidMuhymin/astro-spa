ec4b85f66ae76e283fb566e21b6c4d8f5bf3e77f
RafidMuhymin <rafidmuhymin@gmail.com>
2021-09-22 17:32:33 +0600

    fixed false navigation with hash links

--- navigate.js
+++ navigate.js
@@ -1,6 +1,6 @@
 export default function () {
   return `w.navigate = (href) => {
-    if (l.href !== href && l.pathname !== href) {
+    if (l.href !== href && l.pathname !== href && !l.hash) {
       history.pushState({}, d.title, href);
       constructPage();
     }
