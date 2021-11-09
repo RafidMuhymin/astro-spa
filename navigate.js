6b94931824b3cac3c926b9768f5cea27c361793a
RafidMuhymin <rafidmuhymin@gmail.com>
2021-11-09 16:56:15 +0600

    feat: added config options to navigate API

--- navigate.js
+++ navigate.js
@@ -1,7 +1,7 @@
 export default function () {
-  return `w.navigate = (href) => {
+  return `w.navigate = (href, options) => {
     if (l.href !== href && l.pathname !== href) {
-      history.pushState({}, d.title, href);
+      history[options?.reload ? "replaceState" : "pushState"](options?.state || {}, d.title, href);
       constructPage();
     }
   };`;
