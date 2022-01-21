46040a01f99132e41f29061c04d868cec83600fb
RafidMuhymin <rafidmuhymin@gmail.com>
2022-01-21 18:30:51 +0600

    added buildPage.js

--- constructPage.js
+++ constructPage.js
@@ -1,3 +1,4 @@
+import buildPage from "./buildPage";
 import buildProgressBar from "./progressBar";
 
 export default function (
@@ -12,52 +13,15 @@ export default function (
   scrollIntoView,
   scrollIntoViewOptions
 ) {
-  const buildPage = `${
-    containerSelector
-      ? `const newContent = doc.querySelector("${containerSelector}");
-      ${
-        scrollIntoView
-          ? `const container = document.querySelector("${containerSelector}");
-      container.replaceWith(newContent);
-      newContent.scrollIntoView(${JSON.stringify(scrollIntoViewOptions)});`
-          : ""
-      }
-      document.head.replaceWith(doc.head);
-      ${localLinkDetector ? "styleLocalLink();" : ""}`
-      : "document.documentElement.replaceWith(doc.documentElement);"
-  }
-    [
-      ...document.${
-        containerSelector
-          ? `querySelectorAll("head script, ${containerSelector} script")`
-          : "scripts"
-      }
-    ].forEach((script) => {
-      const newScript = document.createElement("script");
-      newScript.text = script.text;
-      for (const attr of script.attributes) {
-        newScript.setAttribute(attr.name, attr.value);
-      }
-      script.replaceWith(newScript);
-    });
-
-    ${scanOnMount ? "AstroSpa.scan();" : ""}
-    ${analytics ? "AstroSpa.track();" : ""}
-    window.dispatchEvent(new Event("mount"));
-    window.onMount && window.onMount();
-
-    ${
-      defaultAnimation
-        ? `${
-            containerSelector ? "newContent" : "document.documentElement"
-          }.animate(
-        {
-        opacity: [0, 1],
-        },
-        1000
-    );`
-        : ""
-    }`;
+  const buildPage = buildPage(
+    containerSelector,
+    scrollIntoView,
+    scrollIntoViewOptions,
+    localLinkDetector,
+    scanOnMount,
+    analytics,
+    defaultAnimation
+  );
 
   return `const constructPage = async () => {
     window.dispatchEvent(new Event("navigate"));
